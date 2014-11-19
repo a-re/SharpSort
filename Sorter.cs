e46fd5f33724e5c53e2898d462628dadabb136c8
Alex Restifo <calix1999@gmail.com>
2014-11-19 16:53:01 -0500

    First!

---
+++ Sorter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpSort
+{
+    class Sorter
+    {
+        public Sorter() { }
+
+        public int[] BogoSort(int[] input)
+        {
+
+        }
+
+        //ref keyword is like passing a pointer argument :D
+        private void BogoShuffle(ref int[] input)
+        {
+
+        }
+    }
+}
